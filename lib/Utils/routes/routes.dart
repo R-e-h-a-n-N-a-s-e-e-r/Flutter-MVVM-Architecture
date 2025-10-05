ad4a4269c978131d82f37b3757cf14bcfc2c971e
REHAN-NASEER <rehannaseer32123@gmail.com>
2025-10-05 17:44:51 +0500

    Named Routes - Flutter MVVM Architecture

---
+++ lib/Utils/routes/routes.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mvvm_architecture/Utils/routes/routes_name.dart';
+import 'package:flutter_mvvm_architecture/View/home_screen.dart';
+import 'package:flutter_mvvm_architecture/View/login_screen.dart';
+
+class Routes {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case RoutesName.home:
+        return MaterialPageRoute(builder: (context) => HomeScreen());
+      case RoutesName.login:
+        return MaterialPageRoute(builder: (context) => LoginScreen());
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(body: Text('No route defined')),
+        );
+    }
+  }
+}
